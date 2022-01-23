08f644fc6499739d5b8236d848456ac1a160a091
Moritz Lehmann <moritz.lehmann@uni-bayreuth.de>
2022-01-23 22:05:37 +0100

    Inlined remaining functions in opencl.hpp, enabled cl_khr_int64_base_atomics by default

--- src/opencl.hpp
+++ src/opencl.hpp
@@ -26,8 +26,8 @@ struct Device_Info {
 	uint is_fp64_capable=0u, is_fp32_capable=0u, is_fp16_capable=0u, is_int64_capable=0u, is_int32_capable=0u, is_int16_capable=0u, is_int8_capable=0u;
 	uint cores=0u; // for CPUs, compute_units is the number of threads (twice the number of cores with hyperthreading)
 	float tflops=0.0f; // estimated device floating point performance in TeraFLOPs/s
-	Device_Info() = default;
-	Device_Info(const cl::Device& d) {
+	inline Device_Info() = default;
+	inline Device_Info(const cl::Device& d) {
 		cl_device = d; // see https://www.khronos.org/registry/OpenCL/sdk/1.2/docs/man/xhtml/clGetDeviceInfo.html
 		name = trim(d.getInfo<CL_DEVICE_NAME>()); // device name
 		vendor = trim(d.getInfo<CL_DEVICE_VENDOR>()); // device vendor
@@ -59,7 +59,7 @@ struct Device_Info {
 	}
 };
 
-inline string get_opencl_c_code(); // implemented in kernel.hpp
+string get_opencl_c_code(); // implemented in kernel.hpp
 inline void print_device_info(const Device_Info& d, const int id=-1) { // print OpenCL device info
 	println("\r|----------------.--------------------------------------------------|");
 	if(id>-1) println("| Device ID      | "+alignl(48, to_string(id))+" |");
@@ -134,17 +134,20 @@ private:
 	cl::Context cl_context;
 	cl::Program cl_program;
 	cl::CommandQueue cl_queue;
-	string enable_device_capabilities() const { return // enable FP64/FP16 capabilities if available
+	inline string enable_device_capabilities() const { return // enable FP64/FP16 capabilities if available
 		"\n	#ifdef cl_khr_fp64"
 		"\n	#pragma OPENCL EXTENSION cl_khr_fp64 : enable" // make sure cl_khr_fp64 extension is enabled
 		"\n	#endif"
 		"\n	#ifdef cl_khr_fp16"
 		"\n	#pragma OPENCL EXTENSION cl_khr_fp16 : enable" // make sure cl_khr_fp16 extension is enabled
 		"\n	#endif"
+		"\n	#ifdef cl_khr_int64_base_atomics"
+		"\n	#pragma OPENCL EXTENSION cl_khr_int64_base_atomics : enable" // make sure cl_khr_int64_base_atomics extension is enabled
+		"\n	#endif"
 	;}
 public:
 	Device_Info info;
-	Device(const Device_Info& d=select_device_with_most_flops(), const string& opencl_c_code=get_opencl_c_code()) {
+	inline Device(const Device_Info& d=select_device_with_most_flops(), const string& opencl_c_code=get_opencl_c_code()) {
 		info = d;
 		cl_context = cl::Context(info.cl_device);
 		cl_queue = cl::CommandQueue(cl_context, info.cl_device); // queue to push commands for the device
@@ -168,13 +171,13 @@ public:
 		write_file("bin/kernel.ptx", cl_program.getInfo<CL_PROGRAM_BINARIES>()[0]); // save binary (ptx file)
 #endif // PTX
 	}
-	cl::Context get_cl_context() const {
+	inline cl::Context get_cl_context() const {
 		return cl_context;
 	}
-	cl::Program get_cl_program() const {
+	inline cl::Program get_cl_program() const {
 		return cl_program;
 	}
-	cl::CommandQueue get_cl_queue() const {
+	inline cl::CommandQueue get_cl_queue() const {
 		return cl_queue;
 	}
 };
@@ -189,7 +192,7 @@ private:
 	cl::Buffer device_buffer; // device buffer
 	cl::CommandQueue cl_queue;
 public:
-	Memory(const Device& device, const ulong N, const uint dimensions=1u, const bool allocate_host=true, const bool allocate_device=true) {
+	inline Memory(const Device& device, const ulong N, const uint dimensions=1u, const bool allocate_host=true, const bool allocate_device=true) {
 		cl_queue = device.get_cl_queue();
 		this->N = N;
 		this->d = dimensions;
@@ -204,7 +207,7 @@ public:
 		}
 		write_to_device();
 	}
-	Memory(const Device& device, const ulong N, const uint dimensions, T* const host_buffer, const bool allocate_device=true) {
+	inline Memory(const Device& device, const ulong N, const uint dimensions, T* const host_buffer, const bool allocate_device=true) {
 		cl_queue = device.get_cl_queue();
 		this->N = N;
 		this->d = dimensions;
@@ -216,7 +219,7 @@ public:
 		}
 		write_to_device();
 	}
-	~Memory() {
+	inline ~Memory() {
 		delete[] host_buffer;
 		device_buffer = nullptr;
 	}
@@ -282,25 +285,25 @@ private:
 		cl_range_global = cl::NDRange(((N+THREAD_BLOCK_SIZE-1)/THREAD_BLOCK_SIZE)*THREAD_BLOCK_SIZE); // make global range a multiple of local range
 	}
 public:
-	template<typename... T> Kernel(const Device& device, const ulong N, const string& name, const Memory<T>&... parameters) {
+	template<typename... T> inline Kernel(const Device& device, const ulong N, const string& name, const Memory<T>&... parameters) {
 		cl_kernel = cl::Kernel(device.get_cl_program(), name.c_str());
 		(cl_kernel.setArg(number_of_parameters++, parameters.get_cl_buffer()), ...); // expand variadic template to link buffers against kernel parameters
 		initialize_ranges(N);
 		cl_queue = device.get_cl_queue();
 	}
-	template<typename... T> Kernel(const Device& device, const ulong N, const string& name, const Memory<T>*... parameters) {
+	template<typename... T> inline Kernel(const Device& device, const ulong N, const string& name, const Memory<T>*... parameters) {
 		cl_kernel = cl::Kernel(device.get_cl_program(), name.c_str());
 		(cl_kernel.setArg(number_of_parameters++, parameters->get_cl_buffer()), ...); // expand variadic template to link buffers against kernel parameters
 		initialize_ranges(N);
 		cl_queue = device.get_cl_queue();
 	}
-	template<typename T> void add_parameter(const Memory<T>& parameter) {
+	template<typename T> inline void add_parameter(const Memory<T>& parameter) {
 		cl_kernel.setArg(number_of_parameters++, parameter.get_cl_buffer());
 	}
-	template<typename T> void add_parameter(const Memory<T>* parameter) {
+	template<typename T> inline void add_parameter(const Memory<T>* parameter) {
 		cl_kernel.setArg(number_of_parameters++, parameter->get_cl_buffer());
 	}
-	template<typename T> void add_parameter(const T parameter) {
+	template<typename T> inline void add_parameter(const T parameter) {
 		cl_kernel.setArg(number_of_parameters++, parameter);
 	}
 	inline void run() const {
