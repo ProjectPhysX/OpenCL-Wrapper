6c2941e3f343886f921fd6f993e84ca6636e30b8
Moritz Lehmann <moritz.lehmann@uni-bayreuth.de>
2022-09-19 18:11:42 +0200

    Minor additions, added proper TFLOPs/s estimate for Apple GPUs

--- make.sh
+++ make.sh
@@ -1,5 +1,6 @@
 mkdir -p bin # create directory for executable
 rm -f ./bin/OpenCL-Wrapper.exe # prevent execution of old version if compiling fails
 g++ ./src/*.cpp -o ./bin/OpenCL-Wrapper.exe -std=c++17 -pthread -I./src/OpenCL/include -L./src/OpenCL/lib -lOpenCL # compile on Linux
+g++ ./src/*.cpp -o ./bin/OpenCL-Wrapper.exe -std=c++17 -pthread -I./src/OpenCL/include -framework OpenCL # compile on macOS
 #g++ ./src/*.cpp -o ./bin/OpenCL-Wrapper.exe -std=c++17 -pthread -I./src/OpenCL/include -L/system/vendor/lib64 -lOpenCL # compile on Android
 ./bin/OpenCL-Wrapper.exe # run executable
